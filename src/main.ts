344e368b49d3da127b60f7d107ff8cd0e191b0f7
Kalash Shrestha <69349296+kalexrt@users.noreply.github.com>
2024-06-10 22:07:35 +0545

    added buttons

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
 import './style.css'
+
+const images = document.querySelectorAll<HTMLImageElement>('.carousel-image-wrapper img');
+const prevButton = document.querySelector<HTMLButtonElement>('.prev-button');
+const nextButton = document.querySelector<HTMLButtonElement>('.next-button');
+
+
+console.log(prevButton, nextButton, images)
