73282db1d1ff945714e008b00cc55804a9d4fa04
Kalash Shrestha <69349296+kalexrt@users.noreply.github.com>
2024-06-10 23:26:08 +0545

    add indicator and indicator movement

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,49 @@
 import './style.css'
 
-const images = document.querySelectorAll<HTMLImageElement>('.carousel-image-wrapper img');
 const prevButton = document.querySelector<HTMLButtonElement>('.prev-button');
 const nextButton = document.querySelector<HTMLButtonElement>('.next-button');
 const imageWrapper = document.querySelector<HTMLDivElement>('.carousel-image-wrapper');
+const indicators = document.querySelectorAll<HTMLButtonElement>('.indicator');
 
-let currentIndex = -2;
+let currIndex = -2;
+
 
 function updateCarousel() {
   if (imageWrapper) {
-    const offset = -currentIndex * 500; // Assuming each image is 500px wide
+    const offset = -currIndex * 500; // Assuming each image is 500px wide
     imageWrapper.style.transform = `translateX(${offset}px)`;
+    updateIndicators();
   }
 }
 
 prevButton?.addEventListener('click', () => {
-  currentIndex = currentIndex - 1 ;
-  if(currentIndex < -2){
-    currentIndex = 2;
+  currIndex = currIndex - 1 ;
+  if(currIndex < -2){
+    currIndex = 2;
   };
   updateCarousel();
 });
 
 nextButton?.addEventListener('click', () => {
-  currentIndex = currentIndex + 1;
-  if(currentIndex > 2){
-    currentIndex = -2;
+  currIndex = currIndex + 1;
+  if(currIndex > 2){
+    currIndex = -2;
   };
   updateCarousel();
 });
+
+indicators.forEach((indicator, index) => {
+  indicator.addEventListener('click', () => {
+    currIndex = index - 2;
+    updateCarousel();
+  });
+});
+
+function updateIndicators() {
+  indicators.forEach((indicator, index) => {
+    indicator.classList.toggle('active', index - 2 === currIndex);
+  });
+}
+
 
 updateCarousel();
